19caa893927d246ab98170631f59fbbf76bfa274
Nefari0uss <nnefari0uss@gmail.com>
2018-07-06 11:39:21 -0400

    changed boxes to image containers, added backwards button, ability to create and delete boxes

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,74 @@
 import "../sass/_styles";
 
 let container = document.querySelector(".flex-container");
-let flexItems = document.querySelectorAll(".flex-item");
+let flexItems = () => { return document.querySelectorAll(".flex-item");};
+let ri = "reference-item";
+
+function getFlexItemsCount() {
+    return flexItems().length == null ? 0 : flexItems().length;
+}
+
+function createItem() {
+    console.log("Creating item.");
+
+    let fi = document.createElement("div");
+    fi.classList.add("flex-item");
+
+    if (getFlexItemsCount() == 0) {
+        fi.classList.add("reference-item");
+    }
+
+    let div = document.createElement("div");
+    div.classList.add("img-container");
+
+    let img = document.createElement("img");
+    img.src = "https://cdnb.artstation.com/p/assets/images/images/005/060/083/large/astri-lohne-sjursen-mononokeb.jpg?1488213693";
+    img.alt = "Princess Monomuke";
+    div.appendChild(img);
+
+    let div2 = document.createElement("div");
+    div.classList.add("cta-container");
+    div2.innerHTML = getFlexItemsCount().toString();
+
+    fi.appendChild(div);
+    fi.appendChild(div2);
+    container.appendChild(fi);
+}
+
+function deleteItem() {
+    console.log("Deleting item");
+    let length = getFlexItemsCount();
+    if (length != 0) {
+        container.removeChild(flexItems()[length - 1]);
+    }
+}
 
 function getNextItem(el) {
-    if (el.nextElementSibling == null) {
-        return flexItems[0];
-    } else {
-        return el.nextElementSibling;
-    }
+    return el.nextElementSibling == null ? flexItems()[0] : el.nextElementSibling;
 }
 
 function getPreviousItem(el) {
-    if (el.previousElementSibling == null) {
-        return flexItems[flexItems.length - 1];
+    return el.previousElementSibling == null ? flexItems()[getFlexItemsCount() - 1] : el.previousElementSibling;
+}
+
+function transition(num) {
+    let stasis = "stasis";
+    let forward = "slide-forward";
+    let backward = "slide-backward";
+
+    if (num == 1) {
+        container.classList.remove(backward);
+        container.classList.add(forward);
     } else {
-        return el.previousElementSibling;
+        container.classList.remove(forward);
+        container.classList.add(backward);
     }
+    
+    container.classList.remove(stasis);
+    setTimeout( () => { container.classList.add(stasis); }, 50);
 }
-
-function transitionForward() {
-    container.classList.remove("transition-forward");
-    setTimeout( () => { container.classList.add("transition-forward"); }, 50);
-}
-
-
 
 function goForward() {
-    let ri = "reference-item";
     let currentItem = document.getElementsByClassName(ri)[0];
     currentItem.classList.remove(ri);
 
@@ -39,17 +80,30 @@ function goForward() {
     document.querySelector("#is").innerText = currentItem.innerText;
 
     incrementOrder(currentItem);
-    transitionForward();
+    transition(1);
 }
 
-function goBack() { console.log("go back"); }
+function goBack() {
+    let currentItem = document.getElementsByClassName(ri)[0];
+    currentItem.classList.remove(ri);
+
+    document.querySelector("#was").innerText = currentItem.innerText;
+
+    currentItem = getPreviousItem(currentItem);
+    currentItem.classList.add(ri);
+
+    document.querySelector("#is").innerText = currentItem.innerText;
+
+    incrementOrder(currentItem);
+    transition(0);
+
+}
 
 function incrementOrder(el) {
     console.log("in increment order");
-    //console.log("el:" + el);
 
     let item = el;
-    for (let i = 0; i < flexItems.length; i++) {
+    for (let i = 0; i < getFlexItemsCount(); i++) {
         item.style.order = i.toString();
         item = getNextItem(item);
     }
@@ -76,8 +130,6 @@ function changeOrderBack() {
 
     console.log("fin\n");
 }
-
-
 
 function changeOrder() {
     let boxes = document.querySelectorAll(".flex-item");
@@ -112,15 +164,19 @@ function printOrder() {
     console.log("fin\n");
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
-    let backButton = document.querySelector("#backButton");
 
-    let forwardButton = document.querySelector("#forwardButton");
+    for(let i = 0; i < 6; i++) {
+        createItem();
+    }
 
-    backButton.addEventListener("click", () => { goBack(); });
+    document.querySelector("#backButton").addEventListener("click", () => { goBack(); });
 
-    forwardButton.addEventListener("click", () => { goForward(); });
+    document.querySelector("#forwardButton").addEventListener("click", () => { goForward(); });
 
     document.querySelector("#printOrder").addEventListener("click", () => { printOrder(); });
+
+    document.querySelector("#createElement").addEventListener("click", () => { createItem(); });
+
+    document.querySelector("#deleteElement").addEventListener("click", () => { deleteItem(); });
 });
